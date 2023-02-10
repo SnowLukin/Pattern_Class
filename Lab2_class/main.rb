90c25241d2f8c9ab00f38a341d780c57e6706160
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-10 14:54:20 +0300

    Fixed creating students

--- Lab2_class/main.rb
+++ Lab2_class/main.rb
@@ -1,9 +1,9 @@
 require_relative 'Lab2'
 
 def create_three_students
-  student1 = Student.new(surname: 'Andrewwww', first_name: 'Andrew', git: 'https://github.com/Andrew')
-  student2 = Student.new(surname: 'Boooob', first_name: 'Bob', phone: '1234567890', mail: 'Bob@gmail.com')
-  student3 = Student.new(surname: 'Steeeve', first_name: 'Steve', phone: '1234567890', mail: 'Steve@mail.ru', git: 'https://github.com/Steve')
+  student1 = Student.new(surname:'Andreeew', name:'Andrew', middle_name:'dsf', git: 'https://github.com/Andrew')
+  student2 = Student.new(surname:'Boooob', name:'Bob', middle_name:'asdas', phone: '+1234567890', email: 'Bob@gmail.com')
+  student3 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
   [student1, student2, student3]
 end
 
