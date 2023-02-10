862861744ef201807ba54baac02f65bf3df73a45
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-10 15:14:58 +0300

    Added test for from_string constructor

--- Lab2_class/main.rb
+++ Lab2_class/main.rb
@@ -1,10 +1,11 @@
 require_relative 'Lab2'
 
-def create_three_students
+def create_four_students
   student1 = Student.new(surname:'Andreeew', name:'Andrew', middle_name:'dsf', git: 'https://github.com/Andrew')
   student2 = Student.new(surname:'Boooob', name:'Bob', middle_name:'asdas', phone: '+1234567890', email: 'Bob@gmail.com')
   student3 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
-  [student1, student2, student3]
+  student4 = Student.from_string('1,Andreeew,Andrew,dsf')
+  [student1, student2, student3, student4]
 end
 
 def print_students(students)
@@ -15,10 +16,10 @@ end
 
 def main
   # Create three students
-  student1, student2, student3 = create_three_students
+  student1, student2, student3, student4 = create_four_students
 
   # Print their data
-  print_students([student1, student2, student3])
+  print_students([student1, student2, student3, student4])
 end
 
 main
