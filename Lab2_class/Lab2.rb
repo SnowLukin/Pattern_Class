24408010c81993e5c8ae11b0851e182c6ffaddac
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-10 13:18:57 +0300

    Validate phone number

--- Lab2_class/Lab2.rb
+++ Lab2_class/Lab2.rb
@@ -5,6 +5,9 @@
 class Student
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
+  # Phone number 10 - 11 digits
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/
+
   # Init with any number of params
   def initialize(params = {})
     @id = params[:id]
@@ -15,10 +18,19 @@ class Student
     @telegram = params[:telegram]
     @email = params[:email]
     @git = params[:git]
+
+    # Validate phone number
+    if @phone && !Student.valid_phone?(@phone)
+      raise ArgumentError, "Неверный формат телефона"
+    end
   end
 
   def to_s
     "ID: #{@id}, Фамилия: #{@surname}, Имя: #{@name}, Отчество: #{@patronymic}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}"
+  end
+
+  def self.valid_phone?(phone)
+    phone =~ VALID_PHONE_REGEX
   end
 
 end
