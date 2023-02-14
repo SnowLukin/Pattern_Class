20b83c88c4b3753fb983b58f0a6b1806d9ec9dad
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-14 15:57:14 +0300

    Update Lab1.rb

--- Lab1_class/Lab1.rb
+++ Lab1_class/Lab1.rb
@@ -1,1 +1,15 @@
+# Задание 1
 
+# Минимальный общий делитель
+def gcd(a, b)
+  b.zero? ? a : gcd(b, a % b)
+end
+
+# Найти количество чисел, взаимно простых с заданным.
+def find_coprimes(num)
+  count = 0
+  (1..num).each do |i|
+    count += 1 if gcd(num, i) == 1
+  end
+  count
+end
