c110c8aed60a7da1766e9762588ab55540a9ef6b
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-21 02:15:19 +0300

    Added read_from_file method

--- Lab2_class/Student.rb
+++ Lab2_class/Student.rb
@@ -12,6 +12,7 @@
 # ---------------------
 # | + initialize(params: Hash)   |
 # | + to_s: String                |
+# | + read_from_txt(filePath: String |
 # | + valid_phone?(phone: String)|
 # | + valid_email?(email: String)|
 # | + valid_git?(git: String)    |
@@ -38,148 +39,170 @@
 require_relative 'Student_super'
 
 class Student < Student_super
-  # Create getters and setters for all instance variables
-  attr_accessor :name, :middle_name, :phone, :telegram, :email
-
-  # Email regex
-  # Email address must be in the following format:
-  #  - 1 or more characters
-  # - @
-  # - 1 or more characters
-  # - .
-  # - 1 or more characters
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
-  # Phone number regex
-  # Phone number must be in the following format:
-  # - one plus sign
-  # - 10 digits
-  # - no spaces
-  # - no letters
-  # - no special characters
-  # - no dashes
-  # - no parentheses
-  # - no periods
-  VALID_PHONE_REGEX = /\A\+?\d{10}\z/
-
-  # Git regex
-  # Git must be in the following format:
-  # - https://github.com
-  # - 1 or more characters
-  # - no spaces
-  # - no special characters
-  # - no dashes
-  VALID_GIT_REGEX = /\Ahttps:\/\/github\.com\/\w+\z/
-
-  # Init with any number of params
-  def initialize(params = {})
-    super.initialize(params[:id], params[:surname], params[:git])
-    @name = params[:name]
-    @middle_name = params[:middle_name]
-    @phone = params[:phone]
-    @telegram = params[:telegram]
-    @email = params[:email]
-
-    # name, surname and middle_name required
-    raise ArgumentError, "Name, surname and middle_name are required" unless @name && @surname && @middle_name
-
+    # Create getters and setters for all instance variables
+    attr_accessor :name, :middle_name, :phone, :telegram, :email
+    
+    # Email regex
+    # Email address must be in the following format:
+    #  - 1 or more characters
+    # - @
+    # - 1 or more characters
+    # - .
+    # - 1 or more characters
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    
+    # Phone number regex
+    # Phone number must be in the following format:
+    # - one plus sign
+    # - 10 digits
+    # - no spaces
+    # - no letters
+    # - no special characters
+    # - no dashes
+    # - no parentheses
+    # - no periods
+    VALID_PHONE_REGEX = /\A\+?\d{10}\z/
+    
+    # Git regex
+    # Git must be in the following format:
+    # - https://github.com
+    # - 1 or more characters
+    # - no spaces
+    # - no special characters
+    # - no dashes
+    VALID_GIT_REGEX = /\Ahttps:\/\/github\.com\/\w+\z/
+    
+    # Init with any number of params
+    def initialize(params = {})
+        super.initialize(params[:id], params[:surname], params[:git])
+        @name = params[:name]
+        @middle_name = params[:middle_name]
+        @phone = params[:phone]
+        @telegram = params[:telegram]
+        @email = params[:email]
+        
+        # name, surname and middle_name required
+        raise ArgumentError, "Name, surname and middle_name are required" unless @name && @surname && @middle_name
+        
+        # Validate phone number
+        if @phone && !Student.valid_phone?(@phone)
+            raise ArgumentError, "Phone in wrong format."
+        end
+        
+        # Validate email
+        if @email && !Student.valid_email?(@email)
+            raise ArgumentError, "Email in wrong format."
+        end
+        
+        # Validate git
+        if @git && !Student.valid_git?(@git)
+            raise ArgumentError, "Git in wrong format."
+        end
+        
+        # Validate that at least one contact is present and git is present
+        validate
+    end
+    
+    # Override to_s method
+    def to_s
+        "ID: #{@id}, Surname: #{@surname}, Name: #{@name}, Middle Name: #{@middle_name}, Phone: #{@phone}, Telegram: #{@telegram}, Email: #{@email}, Git: #{@git}"
+    end
+    
+    # From string to Student object
+    def self.from_string(string)
+        
+        # Error if the string doesnt have at least 3 elements
+        raise ArgumentError, "Wrong number of arguments" unless string.split(',').length >= 3
+        
+        # string = string.gsub(/\s+/, '') # remove whitespaces from string
+        id, surname, name, middle_name, phone, telegram, email, git = string.split(',')
+        params = { id: id, surname: surname, name: name, middle_name: middle_name, phone: phone, telegram: telegram, email: email, git: git }
+        new(params)
+    end
+    
+    # Reads student data from a text file and returns an array of Student objects.
+    #
+    # @param file_path [String] the path to the text file to be read
+    # @return [Array<Student>] an array of Student objects created from the data in the file
+    # @raise [RuntimeError] if the file cannot be found or there is an error while reading it
+    def self.read_from_txt(file_path)
+        students = []
+        begin
+            File.open(file_path, 'r') do |file|
+                file.each_line do |line|
+                    id, surname, first_name, patronymic, phone, telegram, mail, git = line.split(',')
+                    params_to = { id: id, surname: surname, first_name: first_name, patronymic: patronymic, phone: phone, telegram: telegram, mail: mail, git: git }
+                    Student.new(params_to) << students
+                    
+                end
+            end
+            students
+            rescue => exception
+            raise "File not found at the given address #{file_path}. Exception: #{exception.message}"
+        end
+    end
+    
     # Validate phone number
-    if @phone && !Student.valid_phone?(@phone)
-      raise ArgumentError, "Phone in wrong format."
+    def self.valid_phone?(phone)
+        phone =~ VALID_PHONE_REGEX
     end
-
+    
     # Validate email
-    if @email && !Student.valid_email?(@email)
-      raise ArgumentError, "Email in wrong format."
+    def self.valid_email?(email)
+        email =~ VALID_EMAIL_REGEX
     end
-
+    
     # Validate git
-    if @git && !Student.valid_git?(@git)
-      raise ArgumentError, "Git in wrong format."
+    def self.valid_git?(git)
+        git =~ VALID_GIT_REGEX
     end
-
+    
+    # Validate that at least one contact is present
+    def validate_contacts_presence
+        return true if phone || telegram || email
+        puts "At least one contact (phone, telegram, mail) is required"
+        false
+    end
+    
+    # Validate that git is present
+    def validate_git_presence
+        return true if git
+        puts "Git field is required"
+        false
+    end
+    
     # Validate that at least one contact is present and git is present
-    validate
-  end
-
-  # Override to_s method
-  def to_s
-    "ID: #{@id}, Surname: #{@surname}, Name: #{@name}, Middle Name: #{@middle_name}, Phone: #{@phone}, Telegram: #{@telegram}, Email: #{@email}, Git: #{@git}"
-  end
-
-  # From string to Student object
-  def self.from_string(string)
-
-    # Error if the string doesnt have at least 3 elements
-    raise ArgumentError, "Wrong number of arguments" unless string.split(',').length >= 3
-
-    # string = string.gsub(/\s+/, '') # remove whitespaces from string
-    id, surname, name, middle_name, phone, telegram, email, git = string.split(',')
-    params = { id: id, surname: surname, name: name, middle_name: middle_name, phone: phone, telegram: telegram, email: email, git: git }
-    new(params)
-  end
-
-  # Validate phone number
-  def self.valid_phone?(phone)
-    phone =~ VALID_PHONE_REGEX
-  end
-
-  # Validate email
-  def self.valid_email?(email)
-    email =~ VALID_EMAIL_REGEX
-  end
-
-  # Validate git
-  def self.valid_git?(git)
-    git =~ VALID_GIT_REGEX
-  end
-
-  # Validate that at least one contact is present
-  def validate_contacts_presence
-    return true if phone || telegram || email
-    puts "At least one contact (phone, telegram, mail) is required"
-    false
-  end
-
-  # Validate that git is present
-  def validate_git_presence
-    return true if git
-    puts "Git field is required"
-    false
-  end
-
-  # Validate that at least one contact is present and git is present
-  def validate
-    validate_git_presence && validate_contacts_presence
-  end
-
-  # Set contacts
-  def set_contacts(contacts = {})
-    @phone = contacts[:phone]
-    @telegram = contacts[:telegram]
-    @email = contacts[:email]
-  end
-
-  # Short info about student
-  def get_info
-    "#{get_name_info} #{get_git_info}, #{get_contact_info}"
-  end
-
-  # Name info: Ivanov I.I.
-  def get_name_info
-    "#{@surname} #{@name[0]}.#{@middle_name[0]}."
-  end
-
-  # Git info
-  def get_git_info
-    "Git: #{@git}"
-  end
-
-  # Contact info (phone, telegram or email)
-  def get_contact_info
-    return "Phone: #{@phone}" if @phone
-    return "Telegram: #{@telegram}" if @telegram
-    "Email: #{@email}" if @email
-  end
+    def validate
+        validate_git_presence && validate_contacts_presence
+    end
+    
+    # Set contacts
+    def set_contacts(contacts = {})
+        @phone = contacts[:phone]
+        @telegram = contacts[:telegram]
+        @email = contacts[:email]
+    end
+    
+    # Short info about student
+    def get_info
+        "#{get_name_info} #{get_git_info}, #{get_contact_info}"
+    end
+    
+    # Name info: Ivanov I.I.
+    def get_name_info
+        "#{@surname} #{@name[0]}.#{@middle_name[0]}."
+    end
+    
+    # Git info
+    def get_git_info
+        "Git: #{@git}"
+    end
+    
+    # Contact info (phone, telegram or email)
+    def get_contact_info
+        return "Phone: #{@phone}" if @phone
+        return "Telegram: #{@telegram}" if @telegram
+        "Email: #{@email}" if @email
+    end
 end
 
