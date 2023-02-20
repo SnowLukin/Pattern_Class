ddf7619a76d2b572284ed3e14f11323aac4dcabd
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-21 02:03:12 +0300

    Added documentation

--- Lab2_class/Student_super.rb
+++ Lab2_class/Student_super.rb
@@ -1,12 +1,53 @@
-class Student_super
-  attr_reader :id, :surname, :git
+#+----------------------------------------+
+#|              Student_super              |
+#+----------------------------------------+
+#| - id: string                            |
+#| - surname: string                       |
+#| - git: string                           |
+#+----------------------------------------+
+#| + initialize(id: string, surname: string, git: string) |
+#| + to_s(): string                        |
+#| + id(): string                          |
+#| + surname(): string                     |
+#| + git(): string                         |
+#+----------------------------------------+
 
+# Class: Student_super
+#
+# This class represents a student with an ID, surname, and git username.
+class Student_super
+  # Attribute: id
+  #
+  # A string representing the student ID.
+  attr_reader :id
+
+  # Attribute: surname
+  #
+  # A string representing the student's surname.
+  attr_reader :surname
+
+  # Attribute: git
+  #
+  # A string representing the student's git username.
+  attr_reader :git
+
+  # Method: initialize
+  #
+  # Initializes the id, surname, and git attributes with the given values.
+  #
+  # Parameters:
+  # id - The student ID.
+  # surname - The student's surname.
+  # git - The student's git username.
   def initialize(id, surname, git)
     @id = id
     @surname = surname
     @git = git
   end
 
+  # Method: to_s
+  #
+  # Returns a string representation of the student in the format "ID: [id], Surname: [surname], Git: [git]".
   def to_s
     "ID: #{@id}, Surname: #{@surname}, Git: #{@git}"
   end
