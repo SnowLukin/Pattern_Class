88390c8dd2dc3a46545bb2dc602e4e69ffa957ea
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-02-21 16:07:15 +0300

    Moved from lab3 dir

---
+++ Lab2_class/Data_list.rb
@@ -0,0 +1,40 @@
+class Data_list
+    
+    attr_reader :selected
+    
+    def initialize(data)
+        @data = data
+        @selected = []
+    end
+    
+    def [](index)
+        @data[index]
+    end
+    
+    def size
+        @data.size
+    end
+    
+    def each
+        @data.each { |element| yield element }
+    end
+    
+    def select(number)
+        @selected << number
+    end
+    
+    def get_selected
+        @selected
+    end
+    
+    def get_names
+        raise NotImplementedError, "get_names must be implemented in inheriting class"
+    end
+    
+    def get_data
+        raise NotImplementedError, "get_data must be implemented in inheriting class"
+    end
+    
+    private
+    attr_reader :data
+end
