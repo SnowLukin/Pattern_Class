515148cbeca69b137c19c90b84536e7c94874def
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-03-14 00:59:02 +0300

    Fixes previous tests. Added tests for Data_list_student_short.

--- Lab2_class/main.rb
+++ Lab2_class/main.rb
@@ -1,25 +1,39 @@
+require_relative 'Student_short'
+require_relative 'Student'
+require_relative 'Data_list_Student_short'
 require_relative 'Student'
 
 def create_four_students
-  student1 = Student.new(surname:'Andreeew', name:'Andrew', middle_name:'dsf', git: 'https://github.com/Andrew')
-  student2 = Student.new(surname:'Boooob', name:'Bob', middle_name:'asdas', phone: '+1234567890', email: 'Bob@gmail.com')
-  student3 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
-  student4 = Student.from_string('1,Andreeew,Andrew,dsf')
-  [student1, student2, student3, student4]
+    student1 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
+    student2 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
+    student3 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
+    student4 = Student.new(surname:'Steeeve', name:'Steve', middle_name:'ass', phone: '+1234567890', email: 'Steve@email.ru', git: 'https://github.com/Steve')
+    [student1, student2, student3, student4]
 end
 
 def print_students(students)
-  students.each do |student|
-    puts student
-  end
+    students.each do |student|
+        puts student
+    end
 end
 
-def main
-  # Create three students
-  student1, student2, student3, student4 = create_four_students
-
-  # Print their data
-  print_students([student1, student2, student3, student4])
+def create_student_short
+    Student_short.new(id: '1', surname: 'Surname', git: 'https://github.com/Name', contact: 'Contact')
 end
 
-main
+def test_data_list_student_short
+    student = create_student_short
+    data_list = Data_list_Student_short.new([student])
+    puts data_list.select(0)
+    puts data_list.get_names.to_s
+    puts data_list.get_data
+end
+
+
+def main_test
+    print_students(create_four_students)
+    
+    test_data_list_student_short
+end
+
+main_test
