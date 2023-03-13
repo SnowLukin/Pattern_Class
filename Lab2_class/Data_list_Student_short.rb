b17c2299b6ed630574133f5a0de4bbf60821f338
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-03-13 23:30:33 +0300

    Changed logic of getNames to be according to the task. Removed comments.

--- Lab2_class/Data_list_Student_short.rb
+++ Lab2_class/Data_list_Student_short.rb
@@ -1,29 +1,16 @@
 require_relative 'Data_list'
 
 class Data_list_Student_short < Data_list
-    
-    def initialize(student)
-        super.initialize([student.id, student.surname, student.contact, student.git])
-    end
-    
-    def set_data(new_data)
-        @data = new_data
-    end
-    
-    public
-    def get_names
-        @data.each { |student| p "#{student.name}" }
-    end
-    
-    def select(number)
-        @selected << @data[number].id
-    end
-    
-    def get_selected
-        @selected
-    end
-    
-    def get_data
-        @data.each { |student| p "#{student.name} #{student.contact} #{student.git}" }
-    end
+
+  def select(number)
+    @selected << @data[number].id
+  end
+
+  def get_names
+    %w[id surname initials git contact]
+  end
+
+  def get_data
+    @data.each { |student| p "#{student.id} #{student.name} #{student.contact} #{student.git}" }
+  end
 end
