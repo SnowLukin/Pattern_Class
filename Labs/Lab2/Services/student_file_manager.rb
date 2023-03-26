fac6d77b9d7a53c62577e37b8913f20c678a6682
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-03-26 15:28:59 +0300

    Refactored studentfilemanager. Cosmetics. Fixed misspelled file names.

--- Labs/Lab2/Services/student_file_manager.rb
+++ Labs/Lab2/Services/student_file_manager.rb
@@ -8,27 +8,27 @@ require_relative 'FileStrategies/yaml_file_strategy'
 class StudentFileManager
 	include FileManager
 
-	def initialize(strategy = TXTFileStrategy.new)
-		@strategy = strategy
-	end
-
 	def load_data(source)
+		set_strategy_for(source)
 		@strategy.load_data(source)
 	end
 
 	def save_data(destination, data)
+		set_strategy_for(destination)
 		@strategy.save_data(destination, data)
 	end
 
-	def configure_json
-		@strategy = JSONFileStrategy.new
-	end
-
-	def configure_txt
-		@strategy = TXTFileStrategy.new
-	end
-
-	def configure_yaml
-		@strategy = YAMLFileStrategy.new
+	private
+	def set_strategy_for(file_path)
+		case File.extname(file_path)
+		when '.txt'
+			@strategy = TXTFileStrategy.new
+		when '.json'
+			@strategy = JSONFileStrategy.new
+		when '.yaml'
+			@strategy = YAMLFileStrategy.new
+		else
+			raise "Unknown file type"
+		end
 	end
 end
